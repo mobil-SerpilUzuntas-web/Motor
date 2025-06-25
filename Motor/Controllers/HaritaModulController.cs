a6ec2ce83e502cadc7579b52af2bfb15710a5a50
Serpil Uzuntaş <webiyonet51@gmail.com>
2025-06-25 15:39:33 +0300

    HaritaModulController Index view sayfası yapıldı ve  HaritaModulController  HttpPost hazırlandı

--- Motor/Controllers/HaritaModulController.cs
+++ Motor/Controllers/HaritaModulController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Motor.Models;
 
 namespace Motor.Controllers
 {
@@ -6,9 +7,25 @@ namespace Motor.Controllers
     {
         public IActionResult Index()
         {
+
             return View();
             //Harita Modulu kodlar....
             //Arac Takip Modulü Tamamlandı....
+
+        }
+        [HttpPost]
+        public IActionResult Index(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Veritabanına kaydetme işlemi burada yapılabilir
+                // Örn: _context.Users.Add(model); _context.SaveChanges();
+
+                ViewBag.Message = "Kullanıcı başarıyla eklendi.";
+                return View();
+            }
+
+            return View(model);
         }
     }
 }
